17637f4cabbe5cab15e7860d64ab7868093395c0
Pius Ryan <pius5april@gmail.com>
2026-01-26 03:17:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AnimatedRoutes from './components/AnimatedRoutes';
 import CookieConsent from './components/CookieConsent';
